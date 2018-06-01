141fc7d99c2f11aa7ab352367ca6d18821c9b5dc
GingerPlusPlus <woj.pawlik@gmail.com>
2018-06-01 16:32:18 +0200

    Don't remove messages from banned local admins (#60)

--- handlers/middlewares/kickBanned.js
+++ handlers/middlewares/kickBanned.js
@@ -4,16 +4,28 @@ const dedent = require('dedent-js');
 
 // Utils
 const { link } = require('../../utils/tg');
+const { logError } = require('../../utils/log');
 
 const { replyOptions } = require('../../bot/options');
+
+const errorDescriptions = {
+	banAdmin: 'Bad Request: user is an administrator of the chat',
+};
 
 const kickBannedHandler = async (ctx, next) => {
 	if (ctx.chat.type === 'private') {
 		return next();
 	}
 	if (ctx.from.status === 'banned') {
+		try {
+			await ctx.kickChatMember(ctx.from.id);
+		} catch (err) {
+			if (err.description !== errorDescriptions.banAdmin) {
+				logError(err);
+			}
+			return next();
+		}
 		ctx.deleteMessage();
-		await ctx.kickChatMember(ctx.from.id);
 		return ctx.replyWithHTML(
 			dedent(`
 			🚫 ${link(ctx.from)} <b>is banned</b>!
